f303634e290f2990b81d2792638392c1b5e1ba5f
Ben Jacobs <ben.jacobs@vanti.co.uk>
2019-01-28 22:45:18 +0000

    [client] Add DateEditor

---
+++ client/src/components/DateEditor.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-dialog
+    ref="dialog"
+    v-model="dialog"
+    :return-value.sync="date"
+    persistent
+    lazy
+    full-width
+    width="290px"
+  >
+    <v-text-field
+      slot="activator"
+      v-model="date"
+      :label="label"
+      prepend-icon="event"
+      readonly
+    ></v-text-field>
+    <v-date-picker v-model="date" scrollable color="blue">
+      <v-spacer></v-spacer>
+      <v-btn flat color="blue" @click="dialog = false">Cancel</v-btn>
+      <v-btn flat color="blue" @click="$refs.dialog.save(date)">OK</v-btn>
+    </v-date-picker>
+  </v-dialog>
+</template>
+
+<script>
+  export default {
+    name: 'DateEditor',
+    props: {
+      label: {
+        type: String,
+        default: 'Date'
+      },
+      value: {
+        type: String
+      }
+    },
+    data() {
+      return {
+        dialog: false,
+        date: '00:00'
+      }
+    },
+    mounted() {
+      this.date = this.value;
+    },
+    watch: {
+      value() {
+        this.date = this.value;
+      }
+    },
+    methods: {
+      done() {
+        this.$refs.dialog.save(this.date);
+        this.$emit('input', this.date);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -7,11 +7,13 @@ import store from './store';
 
 Vue.config.productionTip = false;
 
+import DateEditor from './components/DateEditor';
 import Schedule from './components/Schedule';
 import ScheduleRow from './components/ScheduleRow';
 import Status from './components/Status';
 import TimeEditor from './components/TimeEditor';
 
+Vue.component('date-editor', DateEditor);
 Vue.component('schedule', Schedule);
 Vue.component('schedule-row', ScheduleRow);
 Vue.component('status', Status);
