b8852d8480f684af026aff6b188e052d20e88f59
Ben Jacobs <ben.jacobs@vanti.co.uk>
2019-01-28 21:53:00 +0000

    [server] Add sleep section update/delete

--- server/index.js
+++ server/index.js
@@ -1,22 +1,26 @@
 const io = require('socket.io')(3003);
 const fs = require('fs');
 const path = require('path');
-const Sleep = require('./sleep');
 
 let jsonPath = path.join(__dirname, 'sleeps.json');
 console.log(jsonPath);
 
-/**
- * @type {{sleeps, schedule}}
- */
-let data = require(jsonPath);
+let {sleeps, schedule} = require(jsonPath);
 
 /**
- * @type {Sleep[]}
+ * @typedef {Object} SleepSection
+ * @property {Number} id
+ * @property {String} asleep
+ * @property {String} awake
  */
-let sleeps = data.sleeps.map(obj => new Sleep(obj));
 
-let schedule = data.schedule;
+/**
+ * @typedef {Object} Sleep
+ * @property {Number} id
+ * @property {String} start
+ * @property {String} end
+ * @property {SleepSection[]} sections
+ */
 
 /**
  * @param {Number} id
@@ -35,6 +39,47 @@ function sleepIndexById(id) {
   return sleeps.findIndex(sleep => sleep.id === id);
 }
 
+/**
+ * @param {Sleep} sleep
+ * @param {Number} id
+ * @return {Number}
+ */
+function sleepSectionById(sleep, id) {
+  return sleep.sections.find(section => section.id === id);
+}
+
+/**
+ * @param {Sleep} sleep
+ * @param {Number} id
+ * @return {Number}
+ */
+function sleepSectionIndexById(sleep, id) {
+  return sleep.sections.findIndex(section => section.id === id);
+}
+
+function updateFrom(a, b, msg) {
+  if (b.id !== a.id) {
+    console.error(`updateFrom error: ids do not match (this id:${a.id} != that id:${b.id})`);
+    return;
+  }
+
+  let message = `${msg} id: ${a.id} updated`;
+
+  Object.keys(this)
+      .forEach(key => {
+        if (key === 'id') {
+          return;
+        }
+
+        if (a[key] !== b[key]) {
+          a[key] = b[key];
+          message += ` ${key} (${a[key]})`;
+        }
+      });
+
+  console.log(message);
+}
+
 io.on('connection', socket => {
   console.log('client connected');
   // send all data to client
@@ -47,16 +92,15 @@ io.on('connection', socket => {
   }
 
   /**
-   * @param {Object} sleep
+   * @param {Sleep} sleep
    */
   function updateSleep(sleep) {
-    sleep = new Sleep(sleep);
     const localCopy = sleepById(sleep.id);
     if (localCopy === undefined) {
       console.log(`added sleep with id = ${sleep.id}`);
       sleeps.push(sleep);
     } else {
-      localCopy.updateFrom(sleep);
+      updateFrom(localCopy, sleep, `sleep`);
     }
 
     socket.broadcast.emit('update-sleep', sleep);
@@ -77,6 +121,42 @@ io.on('connection', socket => {
     socket.broadcast.emit('delete-sleep', id);
   }
 
+  /**
+   * @param {Number} sleepId
+   * @param {SleepSection} section
+   */
+  function updateSleepSection(sleepId, section) {
+    const sleep = sleepById(sleepId);
+    const localSection = sleepSectionById(sleep, section.id);
+    if (localSection === undefined) {
+      console.log(`added section with id = ${section.id} to sleep id = ${sleepId}`);
+      sleep.sections.push(section);
+    } else {
+      updateFrom(localSection, section, `(sleep id = ${sleepId}) section`);
+    }
+
+    socket.broadcast.emit('update-sleep-section', sleepId, section);
+    save();
+  }
+
+  /**
+   * @param {Number} sleepId
+   * @param {Number} sectionId
+   */
+  function deleteSleepSection(sleepId, sectionId) {
+    const sleep = sleepById(sleepId);
+    const index = sleepSectionIndexById(sleep, sectionId);
+    if (index === -1) {
+      console.warn(`unable to find section with id = ${id} in sleep id = ${sleepId} to delete`);
+    } else {
+      console.log(`deleted section with id = ${id} in sleep id = ${sleepId}`);
+      sleep.sections.splice(index, 1);
+    }
+
+    socket.broadcast.emit('delete-sleep-section', sleepId, sectionId);
+    save();
+  }
+
   function updateSchedule(s) {
     console.log('update schedule');
     schedule = s;
@@ -84,9 +164,12 @@ io.on('connection', socket => {
     save();
   }
 
+
   socket.on('refresh', refresh);
   socket.on('update-sleep', updateSleep);
   socket.on('delete-sleep', deleteSleep);
+  socket.on('update-sleep-section', updateSleepSection);
+  socket.on('delete-sleep-section', deleteSleepSection);
   socket.on('update-schedule', updateSchedule);
 });
 
